63b244b67342df4613592e5c96bb019e9ed9a86e
Eden Ghirmai <edeng94@gmail.com>
2024-11-08 17:18:46 -0800

    add hints

--- src/games/WordyVerse/Topics/NYTGuild/Data.js
+++ src/games/WordyVerse/Topics/NYTGuild/Data.js
@@ -1,100 +1,100 @@
 export const WORDS = [
-  {
-    date: "2024-11-23",
-    theme: "Union",
-    word: "POLICY",
-    characterCount: 6,
-  },
-  {
-    date: "2024-11-22",
-    theme: "Union",
-    word: "BOYCOTT",
-    characterCount: 7,
-  },
-  {
-    date: "2024-11-21",
-    theme: "Union",
-    word: "WAGES",
-    characterCount: 5,
-  },
-  {
-    date: "2024-11-20",
-    theme: "Union",
-    word: "TERMS",
-    characterCount: 5,
-  },
-  {
-    date: "2024-11-19",
-    theme: "Union",
-    word: "FUNDS",
-    characterCount: 5,
-  },
-  {
-    date: "2024-11-18",
-    theme: "Union",
-    word: "MEMBER",
-    characterCount: 6,
-  },
-  {
-    date: "2024-11-17",
-    theme: "Union",
-    word: "UNION",
-    characterCount: 5,
-  },
-  {
-    date: "2024-11-16",
-    theme: "Union",
-    word: "LABOR",
-    characterCount: 5,
-  },
-  {
-    date: "2024-11-15",
-    theme: "Union",
-    word: "PICKET",
-    characterCount: 6,
-  },
-  {
-    date: "2024-11-14",
-    theme: "Union",
-    word: "EQUITY",
-    characterCount: 6,
-  },
-  {
-    date: "2024-11-13",
-    theme: "Union",
-    word: "STRONG",
-    characterCount: 6,
-  },
-  {
-    date: "2024-11-12",
-    theme: "Union",
-    word: "BARGAIN",
-    characterCount: 7,
-  },
-  {
-    date: "2024-11-11",
-    theme: "Union",
-    word: "SCABBY",
-    characterCount: 6,
-  },
-  {
-    date: "2024-11-10",
-    theme: "Union",
-    word: "UNFAIR",
-    characterCount: 6,
-  },
-  {
-    date: "2024-11-09",
-    theme: "Union",
-    word: "GUILD",
-    characterCount: 5,
-  },
-  {
-    date: "2024-11-08",
-    theme: "Union",
-    word: "STRIKE",
-    characterCount: 6,
-  },
+    {
+      date: '2024-11-23',
+      theme: '',
+      word: 'POLICY',
+      characterCount: 6
+    },
+    {
+      date: '2024-11-22',
+      theme: '',
+      word: 'BOYCOTT',
+      characterCount: 7
+    },
+    {
+      date: '2024-11-21',
+      theme: '',
+      word: 'WAGES',
+      characterCount: 5
+    },
+    {
+      date: '2024-11-20',
+      theme: '',
+      word: 'TERMS',
+      characterCount: 5
+    },
+    {
+      date: '2024-11-19',
+      theme: '',
+      word: 'FUNDS',
+      characterCount: 5
+    },
+    {
+      date: '2024-11-18',
+      theme: 'An individual who is part of a labor union is a member of that union, ex: "they are a member of the Times Guild"',
+      word: 'MEMBER',
+      characterCount: 6
+    },
+    {
+      date: '2024-11-17',
+      theme: 'A group of workers who join together improve their employment conditions. The NY Times Guild and NY Time Tech Guild are unions',
+      word: 'UNION',
+      characterCount: 5
+    },
+    {
+      date: '2024-11-16',
+      theme: 'What employees give the employer in exchange for wages.',
+      word: 'LABOR',
+      characterCount: 5
+    },
+    {
+      date: '2024-11-15',
+      theme: 'Short for "picketing" an act where people congregate outside a workplace to form a picket line in form of protest',
+      word: 'PICKET',
+      characterCount: 6
+    },
+    {
+      date: '2024-11-14',
+      theme: 'Pay equity is one of the main issues the Times Guild is fighting for.',
+      word: 'EQUITY',
+      characterCount: 6
+    },
+    {
+      date: '2024-11-13',
+      theme: 'Employees have stronger negotiation power with unions',
+      word: 'STRONG',
+      characterCount: 6
+    },
+    {
+      date: '2024-11-12',
+      theme: 'The NYTimes Tech Guild has been bargaining for better working conditions for over two years',
+      word: 'BARGAIN',
+      characterCount: 7
+    },
+    {
+      date: '2024-11-11',
+      theme: 'The name of a "union rat" used as a symbol to call out employers who employe nonunion workers',
+      word: 'SCABBY',
+      characterCount: 6
+    },
+    {
+      date: '2024-11-10',
+      theme: 'Visit the NY Time Guild to find out why workers are striking against unfair labor practices.',
+      word: 'UNFAIR',
+      characterCount: 6
+    },
+    {
+      date: '2024-11-09',
+      theme: 'The name of the NYTimes union is the "Times Guild" which was started in 1940',
+      word: 'GUILD',
+      characterCount: 5
+    },
+    {
+      date: '2024-11-08',
+      theme: 'The NYTime Tech Guild strike began on Nov. 4th 2024',
+      word: 'STRIKE',
+      characterCount: 6
+    }
 ];
 
 export const getWordForDate = (date) => {
