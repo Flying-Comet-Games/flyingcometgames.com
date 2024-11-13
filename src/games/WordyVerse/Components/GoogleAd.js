126d1f84884e04c12cfbea73d7574246b55f95da
Eden Ghirmai <edeng94@gmail.com>
2024-11-13 13:05:22 -0800

    try adesense package

--- src/games/WordyVerse/Components/GoogleAd.js
+++ src/games/WordyVerse/Components/GoogleAd.js
@@ -1,34 +1,49 @@
-import React, { useEffect } from 'react';
+import React from 'react'
+import { Adsense } from '@ctrl/react-adsense';
+// import './css/AdsenseStylesheet.css';
 
 const GoogleAd = ({ slot }) => {
-  useEffect(() => {
-    try {
-      // Add the AdSense script if it hasn't been added yet
-      if (!(window.adsbygoogle || []).push) {
-        const script = document.createElement('script');
-        script.src = 'https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1729981660160138';
-        script.async = true;
-        script.crossOrigin = 'anonymous';
-        document.head.appendChild(script);
-      }
+        //console.log("Ads Shown")
+        return (
+            <Adsense
+                className='adsbygoogle'
+                client="ca-pub-1729981660160138"
+                slot={slot}
+            />
+        )
 
-      // Push the ad
-      (window.adsbygoogle = window.adsbygoogle || []).push({});
-    } catch (error) {
-      console.error('Error loading AdSense:', error);
     }
-  }, []);
-
-  return (
-    <ins
-      className="adsbygoogle"
-      style={{ display: 'block', textAlign: 'center' }}
-      data-ad-client="ca-pub-1729981660160138"
-      data-ad-slot={slot}
-      data-ad-format="auto"
-      data-full-width-responsive="true"
-    />
-  );
-};
 
 export default GoogleAd;
+
+// const GoogleAd = ({ slot }) => {
+//   useEffect(() => {
+//     try {
+//       // Add the AdSense script if it hasn't been added yet
+//       if (!(window.adsbygoogle || []).push) {
+//         const script = document.createElement('script');
+//         script.src = 'https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1729981660160138';
+//         script.async = true;
+//         script.crossOrigin = 'anonymous';
+//         document.head.appendChild(script);
+//       }
+
+//       // Push the ad
+//       (window.adsbygoogle = window.adsbygoogle || []).push({});
+//     } catch (error) {
+//       console.error('Error loading AdSense:', error);
+//     }
+//   }, []);
+
+//   return (
+//     <ins
+//       className="adsbygoogle"
+//       style={{ display: 'block', textAlign: 'center' }}
+//       data-ad-client="ca-pub-1729981660160138"
+//       data-ad-slot={slot}
+//       data-ad-format="auto"
+//       data-full-width-responsive="true"
+//     />
+//   );
+// };
+
