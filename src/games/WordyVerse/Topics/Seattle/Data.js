d2744b1c58c2ea9f6a69e0686de7f82b1d386d94
Eden Ghirmai <edeng94@gmail.com>
2024-12-04 12:04:01 -0800

    add better hints

--- src/games/WordyVerse/Topics/Seattle/Data.js
+++ src/games/WordyVerse/Topics/Seattle/Data.js
@@ -2,372 +2,413 @@ export const SEATTLE_WORDS = [
   // November 2024
   {
     date: "2024-11-15",
-    theme: "Name",
+    theme:
+"This park was named after the Secretary of State who negotiated the Alaska Purchase, featuring a gorgeous lakefront and amphitheater",
     word: "SEWARD",
-    characterCount: 6
+    characterCount: 6,
   },
   {
     date: "2024-11-16",
-    theme: "Name",
+    theme:
+"These springtime blossoms draw thousands of visitors to the UW Quad each March and April",
     word: "CHERRY",
-    characterCount: 6
+    characterCount: 6,
   },
   {
     date: "2024-11-17",
-    theme: "Feature",
+    theme:
+"This engineering marvel in Ballard allows boats to travel between different water levels",
     word: "LOCKS",
-    characterCount: 5
+    characterCount: 5,
   },
   {
     date: "2024-11-18",
-    theme: "Name",
+    theme:
+     "This snow-capped peak is visible from Seattle on clear days, standing as Washington's third-highest mountain",
     word: "BAKER",
-    characterCount: 5
+    characterCount: 5,
   },
   {
     date: "2024-11-19",
-    theme: "Business",
+    theme:
+     "This Alki Beach restaurant offers spectacular water views and renowned seafood dishes",
     word: "SALTYS",
-    characterCount: 6
+    characterCount: 6,
   },
   {
     date: "2024-11-20",
-    theme: "Place",
+    theme:
+     "This stadium, previously known as CenturyLink Field, hosts NFL games and major concerts",
     word: "LUMEN",
-    characterCount: 5
+    characterCount: 5,
   },
   {
     date: "2024-11-21",
-    theme: "Name",
+    theme:
+   "This waterfront neighborhood honors a Nisqually chief who was friendly with early settlers",
     word: "LESCHI",
-    characterCount: 6
+    characterCount: 6,
   },
   {
     date: "2024-11-22",
-    theme: "Place",
+    theme:
+ "Home to Seattle's first public swimming pool, this Central District park remains a community hub",
     word: "PRATT",
-    characterCount: 5
+    characterCount: 5,
   },
   {
     date: "2024-11-23",
-    theme: "Place",
+    theme:
+    "Seattle's oldest Japanese restaurant, serving sushi in the International District since 1904",
     word: "MANEKI",
-    characterCount: 6
+    characterCount: 6,
   },
   {
     date: "2024-11-24",
-    theme: "Place",
+    theme:
+  "Look for the iconic red neon sign of this historic bakery near the waterfront",
     word: "FRANZ",
-    characterCount: 5
+    characterCount: 5,
   },
   {
     date: "2024-11-25",
-    theme: "Name",
+    theme:
+"A major downtown avenue named for a pioneering family who arrived in the 1850s",
     word: "BOREN",
-    characterCount: 5
+    characterCount: 5,
   },
   {
     date: "2024-11-26",
-    theme: "Business",
+    theme:
+   "This retail giant began in Seattle's SODO district, famous for bulk shopping and $1.50 hot dogs",
     word: "COSTCO",
-    characterCount: 6
+    characterCount: 6,
   },
   {
     date: "2024-11-27",
-    theme: "Place",
+    theme:
+"Visit this city park's Japanese gardens for an unmatched view of the Cascades",
     word: "HILLMAN",
-    characterCount: 5
+    characterCount: 7,
   },
   {
     date: "2024-11-28",
-    theme: "Place",
+    theme:
+     "This fine dining institution above Lake Union has hosted celebrities and special occasions since 1950",
     word: "CANLIS",
-    characterCount: 6
+    characterCount: 6,
   },
   {
     date: "2024-11-29",
-    theme: "Business",
+    theme:
+  "A beloved local coffee chain that started in 1993 on Queen Anne Hill",
     word: "LADRO",
-    characterCount: 5
+    characterCount: 5,
   },
   {
     date: "2024-11-30",
-    theme: "Name",
+    theme: "Floating bridges connect Seattle to this Lake Washington island",
     word: "MERCER",
-    characterCount: 6
+    characterCount: 6,
   },
-  // December 2024
   {
     date: "2024-12-01",
-    theme: "Business",
+    theme:
+    "This Capitol Hill restaurant puts an Asian twist on American fast food classics",
     word: "UMAMI",
-    characterCount: 5
+    characterCount: 5,
   },
   {
     date: "2024-12-02",
-    theme: "Other",
+    theme:
+  "These flowers create stunning multicolored fields in the Skagit Valley each spring",
     word: "TULIP",
-    characterCount: 5
+    characterCount: 5,
   },
   {
     date: "2024-12-03",
-    theme: "Phrase",
+    theme:
+"When forecasters predict this weather event, Seattleites clear grocery store shelves",
     word: "FREEZE",
-    characterCount: 6
+    characterCount: 6,
   },
   {
     date: "2024-12-04",
-    theme: "Feature",
+    theme:
+"A unique coffee chain born in Capitol Hill, known for its latte art and minimalist design",
     word: "VOAST",
-    characterCount: 5
+    characterCount: 5,
   },
   {
     date: "2024-12-05",
-    theme: "Feature",
+    theme: "This underground passageway replaced the Alaskan Way Viaduct",
     word: "TUNNEL",
-    characterCount: 6
+    characterCount: 6,
   },
   {
     date: "2024-12-06",
-    theme: "Other",
+    theme:
+"This music movement put Seattle on the cultural map in the early 1990s",
     word: "GRUNGE",
-    characterCount: 6
+    characterCount: 6,
   },
   {
     date: "2024-12-07",
-    theme: "Name",
+    theme:
+     "This volcanic peak creates its own weather systems visible from Seattle",
     word: "BAKER",
-    characterCount: 5
+    characterCount: 5,
   },
   {
     date: "2024-12-08",
-    theme: "Name",
+    theme: "Seattle's WNBA team, four-time league champions",
     word: "STORM",
-    characterCount: 5
+    characterCount: 5,
   },
   {
     date: "2024-12-09",
-    theme: "Thing",
+    theme:
+     "These majestic birds can often be spotted fishing along the Puget Sound shoreline",
     word: "EAGLE",
-    characterCount: 5
+    characterCount: 5,
   },
   {
     date: "2024-12-10",
-    theme: "Acronym",
+    theme:
+   "This Seattle Center museum celebrates popular culture and music history",
     word: "MOPOP",
-    characterCount: 5
+    characterCount: 5,
   },
   {
     date: "2024-12-11",
-    theme: "Other",
+    theme: "The weather condition Seattle is most famous for",
     word: "RAINY",
-    characterCount: 5
+    characterCount: 5,
   },
   {
     date: "2024-12-12",
-    theme: "Name",
+    theme:
+"This stunning Japanese garden in South Seattle was created by a pioneering Asian-American landscaper",
     word: "KUBOTA",
-    characterCount: 6
+    characterCount: 6,
   },
   {
     date: "2024-12-13",
-    theme: "Name",
+    theme:
+     "This martial arts legend is buried in Lake View Cemetery on Capitol Hill",
     word: "BRUCE",
-    characterCount: 5
+    characterCount: 5,
   },
   {
     date: "2024-12-14",
-    theme: "Thing",
+    theme:
+ "Watch these iconic fish leap up the fish ladder at the Ballard Locks",
     word: "SALMON",
-    characterCount: 6
+    characterCount: 6,
   },
   {
     date: "2024-12-15",
-    theme: "Other",
+    theme:
+    "These commercial fishing boats can be seen heading out from Fishermen's Terminal",
     word: "FISHER",
-    characterCount: 6
+    characterCount: 6,
   },
   {
     date: "2024-12-16",
-    theme: "Other",
+    theme:
+  "These playful marine mammals can sometimes be spotted in Elliott Bay",
     word: "OTTER",
-    characterCount: 5
+    characterCount: 5,
   },
   {
     date: "2024-12-17",
-    theme: "Place",
+    theme:
+"This hill offers some of the best city views and was once Seattle's primary LGBTQ+ neighborhood",
     word: "QUEEN",
-    characterCount: 5
+    characterCount: 5,
   },
   {
     date: "2024-12-18",
-    theme: "Name",
+    theme: "These famous trees line the UW Quad and bloom each spring",
     word: "CHERRY",
-    characterCount: 6
+    characterCount: 6,
   },
   {
     date: "2024-12-19",
-    theme: "Name",
+    theme:
+"This performing arts venue near Seattle Center was named for a telecommunications pioneer",
     word: "MCCAW",
-    characterCount: 5
+    characterCount: 5,
   },
   {
     date: "2024-12-20",
-    theme: "Feature",
+    theme:
+"This waterfront area houses hundreds of boats and offers stunning sunset views",
     word: "MARINA",
-    characterCount: 6
+    characterCount: 6,
   },
   {
     date: "2024-12-21",
-    theme: "Place",
+    theme:
+     "This valley north of Seattle is famous for its tulip fields and eagle watching",
     word: "SKAGIT",
-    characterCount: 6
+    characterCount: 6,
   },
   {
     date: "2024-12-22",
-    theme: "Other",
+    theme:
+     "These large public art pieces can be found throughout Pioneer Square and the International District",
     word: "MURAL",
-    characterCount: 5
+    characterCount: 5,
   },
   {
     date: "2024-12-23",
-    theme: "Name",
+    theme:
+    "This street on Capitol Hill is home to many popular restaurants and bars",
     word: "OLIVE",
-    characterCount: 5
+    characterCount: 5,
   },
   {
     date: "2024-12-24",
-    theme: "Place",
+    theme:
+"This island, accessible only by ferry, is known for its artist community and local farms",
     word: "VASHON",
-    characterCount: 6
+    characterCount: 6,
   },
   {
     date: "2024-12-25",
-    theme: "Name",
+    theme: "This Mariners baseball legend has a statue outside T-Mobile Park",
     word: "EDGAR",
-    characterCount: 5
+    characterCount: 5,
   },
   {
     date: "2024-12-26",
-    theme: "Two words",
+    theme:
+"This iconic restaurant near Pike Place Market is known for its coconut cream pies",
     word: "THEPI",
-    characterCount: 5
+    characterCount: 5,
   },
   {
     date: "2024-12-27",
-    theme: "Name",
+    theme:
+"This park offers one of Seattle's best viewpoints of downtown and Mount Rainier",
     word: "KRACKE",
-    characterCount: 6
+    characterCount: 6,
   },
   {
     date: "2024-12-28",
-    theme: "Feature",
+    theme:
+"This historic downtown destination is famous for fish-throwing and local vendors",
     word: "MARKET",
-    characterCount: 6
+    characterCount: 6,
   },
   {
     date: "2024-12-29",
-    theme: "Other",
+    theme: "Watch planes take off and land from the observation deck at Seatac",
     word: "FLIGHT",
-    characterCount: 6
+    characterCount: 6,
   },
   {
     date: "2024-12-30",
-    theme: "Acronym",
+    theme: "This South Lake Union museum preserves and shares local history",
     word: "MOHAI",
-    characterCount: 5
+    characterCount: 5,
   },
   {
     date: "2024-12-31",
-    theme: "Other",
+    theme:
+"This popular medical TV show was set in Seattle but filmed elsewhere",
     word: "GREYS",
-    characterCount: 5
+    characterCount: 5,
   },
-  // January 2025
   {
     date: "2025-01-01",
-    theme: "Two words",
+    theme: "This major downtown street is home to the original Nordstrom store",
     word: "THEAVE",
-    characterCount: 6
+    characterCount: 6,
   },
   {
     date: "2025-01-03",
-    theme: "Business",
+    theme: "This local newspaper has been publishing since 1891",
     word: "TIMES",
-    characterCount: 5
+    characterCount: 5,
   },
   {
     date: "2025-01-04",
-    theme: "Name",
+    theme: "This Ballard street leads to the Ballard Bridge",
     word: "DRAVUS",
-    characterCount: 6
+    characterCount: 6,
   },
   {
     date: "2025-01-05",
-    theme: "Other",
+    theme: "This gold rush trail connected Seattle to Alaska",
     word: "YUKON",
-    characterCount: 5
+    characterCount: 5,
   },
   {
     date: "2025-01-06",
-    theme: "Place",
+    theme: "This park in Ballard features a popular off-leash dog area",
     word: "GOLDEN",
-    characterCount: 6
+    characterCount: 6,
   },
   {
     date: "2025-01-07",
-    theme: "Place",
+    theme:
+     "This botanical attraction near the Space Needle features numerous glass sculptures",
     word: "GARDEN",
-    characterCount: 6
+    characterCount: 6,
   },
   {
     date: "2025-01-08",
-    theme: "Other",
+    theme: "UW's athletic teams are known by this mascot",
     word: "HUSKY",
-    characterCount: 5
+    characterCount: 5,
   },
   {
     date: "2025-01-09",
-    theme: "Business",
+    theme: "This minimalist coffee shop chain started in Capitol Hill",
     word: "VOAST",
-    characterCount: 5
+    characterCount: 5,
   },
   {
     date: "2025-01-10",
-    theme: "Other",
+    theme: "Seattle is known as a hub for this type of beer production",
     word: "CRAFT",
-    characterCount: 5
+    characterCount: 5,
   },
   {
     date: "2025-01-11",
-    theme: "Business",
+    theme:
+     "This historic burger joint in Fremont is known for its walk-up window",
     word: "UNEEDA",
-    characterCount: 6
+    characterCount: 6,
   },
   {
     date: "2025-01-12",
-    theme: "Name",
+    theme:
+     "This street in Capitol Hill is lined with popular bars and restaurants",
     word: "PEARL",
-    characterCount: 5
+    characterCount: 5,
   },
   {
     date: "2025-01-13",
-    theme: "Name",
+    theme: "This distillery in SODO is known for its locally-made spirits",
     word: "BRANDY",
-    characterCount: 6
+    characterCount: 6,
   },
   {
     date: "2025-01-14",
-    theme: "Other",
+    theme:
+     "These flowers bloom throughout the city in summer, especially at the Woodland Park Zoo",
     word: "DAHLIA",
-    characterCount: 6
+    characterCount: 6,
   },
   {
     date: "2025-01-15",
-    theme: "Other",
+    theme: "These NFL birds play their home games at Lumen Field",
     word: "HAWKS",
-    characterCount: 5
-  }
+    characterCount: 5,
+  },
 ];
 
 export const getWordForDate = (date) => {
