9d2aa1bbdf9f7efe27d5dd64406235bd944e53a8
Eden Ghirmai <edeng94@gmail.com>
2024-12-05 10:21:10 -0800

    update gym

--- src/games/WordyVerse/Topics/Gymnastics/Data.js
+++ src/games/WordyVerse/Topics/Gymnastics/Data.js
@@ -1,349 +1,365 @@
-// Data.js
 export const GYMNASTICS_WORDS = [
-    {
-      date: "2024-11-01",
-      theme: "Skill",
-      word: "KIPUP",
-      characterCount: 5,
-    },
-    {
-      date: "2024-11-02",
-      theme: "Equipment",
-      word: "CHALK",
-      characterCount: 5,
-    },
-    {
-      date: "2024-11-03",
-      theme: "Skill",
-      word: "SPLIT",
-      characterCount: 5,
-    },
-    {
-      date: "2024-11-04",
-      theme: "Equipment",
-      word: "VAULT",
-      characterCount: 5,
-    },
-    {
-      date: "2024-11-05",
-      theme: "Skill",
-      word: "TUCKS",
-      characterCount: 5,
-    },
-    {
-      date: "2024-11-06",
-      theme: "Skill",
-      word: "TWIST",
-      characterCount: 5,
-    },
-    {
-      date: "2024-11-07",
-      theme: "Skill",
-      word: "SALTO",
-      characterCount: 5,
-    },
-    {
-      date: "2024-11-08",
-      theme: "Equipment",
-      word: "BEAM",
-      characterCount: 4,
-    },
-    {
-      date: "2024-11-09",
-      theme: "Equipment",
-      word: "RINGS",
-      characterCount: 5,
-    },
-    {
-      date: "2024-11-10",
-      theme: "Equipment",
-      word: "POMMEL",
-      characterCount: 6,
-    },
-    {
-      date: "2024-11-11",
-      theme: "Other",
-      word: "STUCK",
-      characterCount: 5,
-    },
-    {
-      date: "2024-11-12",
-      theme: "Other",
-      word: "FRONT",
-      characterCount: 5,
-    },
-    {
-      date: "2024-11-13",
-      theme: "Skill",
-      word: "LUNGE",
-      characterCount: 5,
-    },
-    {
-      date: "2024-11-14",
-      theme: "Judging",
-      word: "FLARE",
-      characterCount: 5,
-    },
-    {
-      date: "2024-11-15",
-      theme: "Skill",
-      word: "PRESS",
-      characterCount: 5,
-    },
-    {
-      date: "2024-11-16",
-      theme: "Skill",
-      word: "SWING",
-      characterCount: 5,
-    },
-    {
-      date: "2024-11-17",
-      theme: "Other",
-      word: "ROUND",
-      characterCount: 5,
-    },
-    {
-      date: "2024-11-18",
-      theme: "Other",
-      word: "MOUNT",
-      characterCount: 5,
-    },
-    {
-      date: "2024-11-19",
-      theme: "Equipment",
-      word: "HORSE",
-      characterCount: 5,
-    },
-    {
-      date: "2024-11-20",
-      theme: "Other",
-      word: "FLOOR",
-      characterCount: 5,
-    },
-    {
-      date: "2024-11-21",
-      theme: "Skill",
-      word: "AERIAL",
-      characterCount: 6,
-    },
-    {
-      date: "2024-11-22",
-      theme: "Other",
-      word: "TIMER",
-      characterCount: 5,
-    },
-    {
-      date: "2024-11-23",
-      theme: "Equipment",
-      word: "GRIPS",
-      characterCount: 5,
-    },
-    {
-      date: "2024-11-24",
-      theme: "Other",
-      word: "COACH",
-      characterCount: 5,
-    },
-    {
-      date: "2024-11-25",
-      theme: "Skill",
-      word: "SWING",
-      characterCount: 5,
-    },
-    {
-      date: "2024-11-26",
-      theme: "Other",
-      word: "BOARD",
-      characterCount: 5,
-    },
-    {
-      date: "2024-11-27",
-      theme: "Other",
-      word: "PRESS",
-      characterCount: 5,
-    },
-    {
-      date: "2024-11-28",
-      theme: "Other",
-      word: "SCALE",
-      characterCount: 5,
-    },
-    {
-      date: "2024-11-29",
-      theme: "Other",
-      word: "RISE",
-      characterCount: 4,
-    },
-    {
-      date: "2024-11-30",
-      theme: "Skill",
-      word: "TURNS",
-      characterCount: 5,
-    },
-    {
-      date: "2024-12-01",
-      theme: "Judging",
-      word: "STICK",
-      characterCount: 5,
-    },
-    {
-      date: "2024-12-02",
-      theme: "Equipment",
-      word: "UNEVEN",
-      characterCount: 6,
-    },
-    {
-      date: "2024-12-03",
-      theme: "Judging",
-      word: "CLEAN",
-      characterCount: 5,
-    },
-    {
-      date: "2024-12-04",
-      theme: "Other",
-      word: "LEVER",
-      characterCount: 5,
-    },
-    {
-      date: "2024-12-05",
-      theme: "Other",
-      word: "ANGLE",
-      characterCount: 5,
-    },
-    {
-      date: "2024-12-06",
-      theme: "Other",
-      word: "POWER",
-      characterCount: 5,
-    },
-    {
-      date: "2024-12-07",
-      theme: "Skill",
-      word: "JUMPS",
-      characterCount: 5,
-    },
-    {
-      date: "2024-12-08",
-      theme: "Other",
-      word: "DRILLS",
-      characterCount: 6,
-    },
-    {
-      date: "2024-12-09",
-      theme: "Other",
-      word: "SWING",
-      characterCount: 5,
-    },
-    {
-      date: "2024-12-10",
-      theme: "Skill",
-      word: "BRIDGE",
-      characterCount: 6,
-    },
-    {
-      date: "2024-12-11",
-      theme: "Skill",
-      word: "SPRING",
-      characterCount: 6,
-    },
-    {
-      date: "2024-12-12",
-      theme: "Skill",
-      word: "LEAPS",
-      characterCount: 5,
-    },
-    {
-      date: "2024-12-13",
-      theme: "Other",
-      word: "SERIES",
-      characterCount: 6,
-    },
-    {
-      date: "2024-12-14",
-      theme: "Judging",
-      word: "MEETS",
-      characterCount: 5,
-    },
-    {
-      date: "2024-12-15",
-      theme: "Skill",
-      word: "RELEVE",
-      characterCount: 6,
-    },
-    {
-      date: "2024-12-16",
-      theme: "Skill",
-      word: "SALUTE",
-      characterCount: 6,
-    },
-    {
-      date: "2024-12-17",
-      theme: "Judging",
-      word: "CHOREO",
-      characterCount: 6,
-    },
-    {
-      date: "2024-12-18",
-      theme: "Judging",
-      word: "DEDUCT",
-      characterCount: 6,
-    },
-  ];
+  {
+    date: "2024-11-01",
+    theme:
+      "This basic bars skill starts from a dead hang and uses momentum to get above the bar",
+    word: "KIPUP",
+    characterCount: 5,
+  },
+  {
+    date: "2024-11-02",
+    theme:
+      "This powdery substance helps gymnasts maintain their grip on apparatus",
+    word: "CHALK",
+    characterCount: 5,
+  },
+  {
+    date: "2024-11-03",
+    theme:
+      "A position where legs extend in opposite directions, can be done on floor or beam",
+    word: "SPLIT",
+    characterCount: 5,
+  },
+  {
+    date: "2024-11-04",
+    theme:
+      "This apparatus requires a springboard and involves a quick, powerful run",
+    word: "VAULT",
+    characterCount: 5,
+  },
+  {
+    date: "2024-11-05",
+    theme:
+      "These rotations happen in a tight position with knees pulled into chest",
+    word: "TUCKS",
+    characterCount: 5,
+  },
+  {
+    date: "2024-11-06",
+    theme:
+      "This rotation happens around the body's vertical axis, often combined with flips",
+    word: "TWIST",
+    characterCount: 5,
+  },
+  {
+    date: "2024-11-07",
+    theme: "The technical term for a flip, named after a German gymnast",
+    word: "SALTO",
+    characterCount: 5,
+  },
+  {
+    date: "2024-11-08",
+    theme:
+      "This apparatus stands 4 feet off the ground and is only 4 inches wide",
+    word: "BEAM",
+    characterCount: 4,
+  },
+  {
+    date: "2024-11-09",
+    theme:
+      "This men's apparatus requires immense upper body strength to maintain positions",
+    word: "RINGS",
+    characterCount: 5,
+  },
+  {
+    date: "2024-11-10",
+    theme:
+      "This men's apparatus requires continuous circular movements and no stops",
+    word: "POMMEL",
+    characterCount: 6,
+  },
+  {
+    date: "2024-11-11",
+    theme: "When a gymnast nails their landing without moving their feet",
+    word: "STUCK",
+    characterCount: 5,
+  },
+  {
+    date: "2024-11-12",
+    theme:
+      "Direction of rotation where gymnast can see the ground during a flip",
+    word: "FRONT",
+    characterCount: 5,
+  },
+  {
+    date: "2024-11-13",
+    theme:
+      "A step position used for many landings, with one foot forward and knee bent",
+    word: "LUNGE",
+    characterCount: 5,
+  },
+  {
+    date: "2024-11-14",
+    theme: "When a gymnast performs with exceptional artistry and showmanship",
+    word: "FLARE",
+    characterCount: 5,
+  },
+  {
+    date: "2024-11-15",
+    theme:
+      "A strength move where gymnasts push up using shoulder and core strength",
+    word: "PRESS",
+    characterCount: 5,
+  },
+  {
+    date: "2024-11-16",
+    theme:
+      "Basic movement on bars that creates momentum through forward and backward motion",
+    word: "SWING",
+    characterCount: 5,
+  },
+  {
+    date: "2024-11-17",
+    theme: "Type of entry motion that precedes many tumbling passes",
+    word: "ROUND",
+    characterCount: 5,
+  },
+  {
+    date: "2024-11-18",
+    theme: "The skill that begins a routine on any apparatus",
+    word: "MOUNT",
+    characterCount: 5,
+  },
+  {
+    date: "2024-11-19",
+    theme: "The original name for what evolved into modern pommel apparatus",
+    word: "HORSE",
+    characterCount: 5,
+  },
+  {
+    date: "2024-11-20",
+    theme: "This square performance area has special springs underneath",
+    word: "FLOOR",
+    characterCount: 5,
+  },
+  {
+    date: "2024-11-21",
+    theme:
+      "A skill performed without hands touching the ground, like a cartwheel in the air",
+    word: "AERIAL",
+    characterCount: 6,
+  },
+  {
+    date: "2024-11-22",
+    theme: "Device used to track routine length and warm-up periods",
+    word: "TIMER",
+    characterCount: 5,
+  },
+  {
+    date: "2024-11-23",
+    theme:
+      "Hand protection used on bars, with specific finger holes and dowel placement",
+    word: "GRIPS",
+    characterCount: 5,
+  },
+  {
+    date: "2024-11-24",
+    theme: "This person stands nearby during routines for safety and guidance",
+    word: "COACH",
+    characterCount: 5,
+  },
+  {
+    date: "2024-11-25",
+    theme: "The back-and-forth motion that generates momentum on bars",
+    word: "SWING",
+    characterCount: 5,
+  },
+  {
+    date: "2024-11-26",
+    theme:
+      "The springy apparatus used before vault and for mounting other equipment",
+    word: "BOARD",
+    characterCount: 5,
+  },
+  {
+    date: "2024-11-27",
+    theme: "A strength hold where gymnast pushes up into a handstand",
+    word: "PRESS",
+    characterCount: 5,
+  },
+  {
+    date: "2024-11-28",
+    theme:
+      "A balance position where one leg lifts behind while arms extend forward",
+    word: "SCALE",
+    characterCount: 5,
+  },
+  {
+    date: "2024-11-29",
+    theme: "Coming up from a lower position to relevé on toes",
+    word: "RISE",
+    characterCount: 4,
+  },
+  {
+    date: "2024-11-30",
+    theme: "Rotations on one foot, can be done in passé or other positions",
+    word: "TURNS",
+    characterCount: 5,
+  },
+  {
+    date: "2024-12-01",
+    theme: "The perfect landing with no movement of the feet",
+    word: "STICK",
+    characterCount: 5,
+  },
+  {
+    date: "2024-12-02",
+    theme: "These women's bars are set at different heights",
+    word: "UNEVEN",
+    characterCount: 6,
+  },
+  {
+    date: "2024-12-03",
+    theme: "When a skill is performed with perfect form and no deductions",
+    word: "CLEAN",
+    characterCount: 5,
+  },
+  {
+    date: "2024-12-04",
+    theme: "A strength hold where body creates an L-shape or higher",
+    word: "LEVER",
+    characterCount: 5,
+  },
+  {
+    date: "2024-12-05",
+    theme: "Degree of rotation or position relative to apparatus",
+    word: "ANGLE",
+    characterCount: 5,
+  },
+  {
+    date: "2024-12-06",
+    theme: "Explosive strength needed for many gymnastics skills",
+    word: "POWER",
+    characterCount: 5,
+  },
+  {
+    date: "2024-12-07",
+    theme: "Small hops or leaps commonly performed in beam and floor routines",
+    word: "JUMPS",
+    characterCount: 5,
+  },
+  {
+    date: "2024-12-08",
+    theme:
+      "Practice exercises that break down complex skills into manageable parts",
+    word: "DRILLS",
+    characterCount: 6,
+  },
+  {
+    date: "2024-12-09",
+    theme: "Basic movement pattern on bars that creates momentum",
+    word: "SWING",
+    characterCount: 5,
+  },
+  {
+    date: "2024-12-10",
+    theme: "A backbend position with arms and legs straight",
+    word: "BRIDGE",
+    characterCount: 6,
+  },
+  {
+    date: "2024-12-11",
+    theme: "The bouncy action that helps generate height in tumbling",
+    word: "SPRING",
+    characterCount: 6,
+  },
+  {
+    date: "2024-12-12",
+    theme: "Dance elements where both feet leave the ground with legs extended",
+    word: "LEAPS",
+    characterCount: 5,
+  },
+  {
+    date: "2024-12-13",
+    theme: "A combination of connected skills performed in sequence",
+    word: "SERIES",
+    characterCount: 6,
+  },
+  {
+    date: "2024-12-14",
+    theme: "Competitions where multiple teams or individuals compete",
+    word: "MEETS",
+    characterCount: 5,
+  },
+  {
+    date: "2024-12-15",
+    theme: "The French term for rising onto the toes in dance elements",
+    word: "RELEVE",
+    characterCount: 6,
+  },
+  {
+    date: "2024-12-16",
+    theme: "The presentation to judges at start and end of routines",
+    word: "SALUTE",
+    characterCount: 6,
+  },
+  {
+    date: "2024-12-17",
+    theme: "The dance elements and transitions between skills in a routine",
+    word: "CHOREO",
+    characterCount: 6,
+  },
+  {
+    date: "2024-12-18",
+    theme: "Points taken off for form breaks and technical errors",
+    word: "DEDUCT",
+    characterCount: 6,
+  },
+];
 
-  export const getWordForDate = (date) => {
-    if (!date || !GYMNASTICS_WORDS || !Array.isArray(GYMNASTICS_WORDS)) {
-      return null;
-    }
+export const getWordForDate = (date) => {
+  if (!date || !GYMNASTICS_WORDS || !Array.isArray(GYMNASTICS_WORDS)) {
+    return null;
+  }
 
-    // Convert to PT/Los Angeles timezone
-    const ptDate = new Date(
-      date.toLocaleString("en-US", { timeZone: "America/Los_Angeles" })
-    );
+  // Convert to PT/Los Angeles timezone
+  const ptDate = new Date(
+    date.toLocaleString("en-US", { timeZone: "America/Los_Angeles" })
+  );
 
-    // Get current date in PT
-    const currentPTDate = new Date(
-      new Date().toLocaleString("en-US", { timeZone: "America/Los_Angeles" })
-    );
+  // Get current date in PT
+  const currentPTDate = new Date(
+    new Date().toLocaleString("en-US", { timeZone: "America/Los_Angeles" })
+  );
 
-    // Reset time parts for accurate date comparison
-    currentPTDate.setHours(0, 0, 0, 0);
-    ptDate.setHours(0, 0, 0, 0);
+  // Reset time parts for accurate date comparison
+  currentPTDate.setHours(0, 0, 0, 0);
+  ptDate.setHours(0, 0, 0, 0);
 
-    // If requested date is in the future, return null
-    if (ptDate > currentPTDate) {
-      return null;
-    }
+  // If requested date is in the future, return null
+  if (ptDate > currentPTDate) {
+    return null;
+  }
 
-    // Format the PT date as YYYY-MM-DD
-    const dateString =
-      ptDate.getFullYear() +
-      "-" +
-      String(ptDate.getMonth() + 1).padStart(2, "0") +
-      "-" +
-      String(ptDate.getDate()).padStart(2, "0");
+  // Format the PT date as YYYY-MM-DD
+  const dateString =
+    ptDate.getFullYear() +
+    "-" +
+    String(ptDate.getMonth() + 1).padStart(2, "0") +
+    "-" +
+    String(ptDate.getDate()).padStart(2, "0");
 
-    return GYMNASTICS_WORDS.find((entry) => entry.date === dateString);
-  };
+  return GYMNASTICS_WORDS.find((entry) => entry.date === dateString);
+};
 
-  export const findLatestAvailableDate = () => {
-    if (!GYMNASTICS_WORDS || !Array.isArray(GYMNASTICS_WORDS)) {
-      return null;
-    }
+export const findLatestAvailableDate = () => {
+  if (!GYMNASTICS_WORDS || !Array.isArray(GYMNASTICS_WORDS)) {
+    return null;
+  }
 
-    // Get current date in PT
-    const currentPTDate = new Date(
-      new Date().toLocaleString("en-US", { timeZone: "America/Los_Angeles" })
-    );
-    currentPTDate.setHours(0, 0, 0, 0);
+  // Get current date in PT
+  const currentPTDate = new Date(
+    new Date().toLocaleString("en-US", { timeZone: "America/Los_Angeles" })
+  );
+  currentPTDate.setHours(0, 0, 0, 0);
 
-    // Create dates in PT timezone and filter out future dates
-    const sortedEntries = GYMNASTICS_WORDS.map((entry) => ({
-      date: new Date(entry.date + "T00:00:00-08:00"), // Force PT timezone
-      original: entry,
-    }))
-      .filter((entry) => entry.date <= currentPTDate) // Filter out future dates
-      .sort((a, b) => b.date - a.date);
+  // Create dates in PT timezone and filter out future dates
+  const sortedEntries = GYMNASTICS_WORDS.map((entry) => ({
+    date: new Date(entry.date + "T00:00:00-08:00"), // Force PT timezone
+    original: entry,
+  }))
+    .filter((entry) => entry.date <= currentPTDate) // Filter out future dates
+    .sort((a, b) => b.date - a.date);
 
-    return sortedEntries[0]?.original;
-  };
-
+  return sortedEntries[0]?.original;
+};
